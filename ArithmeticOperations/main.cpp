e59a5b3d79a21dfbb2dd17660c5eba451c22ccea
AdrianHugentobler <adrian.hugentobler@hotmail.com>
2021-12-30 09:20:07 +0100

    type conversion

--- ArithmeticOperations/main.cpp
+++ ArithmeticOperations/main.cpp
@@ -4,28 +4,65 @@
 
 int main()
 {
-    //Dimension of the cube
-    float cubeSide = 5.4;
-    //Dimension of sphere
-    float sphereRadius = 2.33;
-    //Dimensions of cone
-    float coneRadius = 7.65;
-    float coneHeight = 14;
 
-    float volCube, volSphere, volCone = 0;
+    int a = 65;
+    char charA = 65;
+    char charB = 'B';
+    float answer = 0;
+    char charC = 67;
+    int integer = 80;
+    float floatNumber = 0.0;
 
-    //Cube Volume = side^3
-    volCube = std::pow(cubeSide, 3);
-    
-    //Sphere Volume = (4/3) * pi * radius^3
-    volSphere = (4 / 3) * M_PI * std::pow(sphereRadius, 3);
+    std::cout << "a = " << a << "\n";
+    std::cout << "charA = " << charA << "\n";
+    std::cout << "charB = " << charB << "\n";
 
-    //Cone Volume = pi * radius^2 * (height/3)
-    volCone = M_PI * std::pow(coneRadius, 2) * (coneHeight / 3);
+    //we can assign an integer to a float
+    floatNumber = integer;
+    std::cout << "integer = " << integer << "\n\n";
+    std::cout << "floatNumber = integer = " << floatNumber << "\n";
 
-    std::cout << "Volume Cube: " << volCube <<"\n";
-    std::cout << "Volume Sphere: " << volSphere <<"\n";
-    std::cout << "Volume Cone: " << volCone <<"\n";
+    //we can assign a char to a float
+    floatNumber = charB;
+    std::cout << "floatNumber = charB = " << floatNumber << "\n";
 
+    answer = floatNumber / 4;
+    std::cout << "answer = floatNumber/4 = " << answer << "\n";
+
+    //But assigning a float to a char doesn't quite work
+    charC = answer;
+    std::cout << "charC = answer = " << charC << "\n";
+
+    //assigning a float to an interger, results in the float being truncated
+    integer = answer;
+    std::cout << "integer = floatNumber = " << integer << "\n";
     return 0;
 }
+
+//int main()
+//{
+//    //Dimension of the cube
+//    float cubeSide = 5.4;
+//    //Dimension of sphere
+//    float sphereRadius = 2.33;
+//    //Dimensions of cone
+//    float coneRadius = 7.65;
+//    float coneHeight = 14;
+//
+//    float volCube, volSphere, volCone = 0;
+//
+//    //Cube Volume = side^3
+//    volCube = std::pow(cubeSide, 3);
+//    
+//    //Sphere Volume = (4/3) * pi * radius^3
+//    volSphere = (4 / 3) * M_PI * std::pow(sphereRadius, 3);
+//
+//    //Cone Volume = pi * radius^2 * (height/3)
+//    volCone = M_PI * std::pow(coneRadius, 2) * (coneHeight / 3);
+//
+//    std::cout << "Volume Cube: " << volCube <<"\n";
+//    std::cout << "Volume Sphere: " << volSphere <<"\n";
+//    std::cout << "Volume Cone: " << volCone <<"\n";
+//
+//    return 0;
+//}
