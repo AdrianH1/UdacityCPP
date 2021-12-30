5e9cde6188657f9f44ec746a74bab0099fdc411e
AdrianHugentobler <adrian.hugentobler@hotmail.com>
2021-12-30 09:34:03 +0100

    assignment operators

--- ArithmeticOperations/main.cpp
+++ ArithmeticOperations/main.cpp
@@ -5,15 +5,37 @@
 void operations();
 void typeConversion();
 void postPreFix();
+void assigmentOperators();
 
 int main() {
     operations();
     typeConversion();
     postPreFix();
+    assigmentOperators();
     return 0;
 }
 
+void assigmentOperators()
+{
+    int a = 0;
+    std::cout << "Variable\t\tOperation\tResult\n";
+    std::cout << "a = " << a;
+    a += 2;
+    std::cout << "\t\t\ta += 2 \t\t a = " << a << "\n";
 
+    std::cout << "a = " << a << " : ";
+    a -= 4;
+    std::cout << "\t\ta -= 4 \t\t a = " << a << "\n";
+
+    int b = 3;
+    std::cout << "a = " << a << ", b = " << b << " : ";
+    a *= b;
+    std::cout << "\ta *= b \t\t a = " << a << "\n";
+
+    std::cout << "a = " << a << ", b = " << b << " : ";
+    a /= b;
+    std::cout << "\ta /= b \t\t a = " << a << "\n";
+}
 
 void postPreFix()
 {
