b0ebfac74f4b76263a50ab6012bbe6c8e9b34a04
Adrian <adrian.hugentobler@hotmail.com>
2022-01-26 09:53:56 +0100

    quiz 1 and 2

---
+++ Vector/Vector/main.cpp
@@ -0,0 +1,27 @@
+#include <vector>
+#include <iostream>
+#include "main.hpp"
+
+int main()
+{
+	std::vector<float> vecfloat;
+	std::cout << "vector has " << vecfloat.size() << " elements" << std::endl;;
+	vecfloat.resize(10);
+	std::cout << "vector has " << vecfloat.size() << " elements" << std::endl;
+
+	/*---------------------------------------------------*/
+
+	std::vector<float> vFloat;
+
+	std::cout << "vFloat has " << vFloat.size() << " elements\n";
+
+	std::cout << "\n\nAdding 10 elements to the vector\n";
+
+	vFloat.assign(10, 8.8);
+
+	std::cout << "vFloat has " << vFloat.size() << " elements\n";
+
+	printVector(vFloat);
+
+	return 0;
+}
