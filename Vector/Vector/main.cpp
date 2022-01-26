a5d8e8c185847a5fe499880104487832f2aeff28
Adrian <adrian.hugentobler@hotmail.com>
2022-01-26 10:19:36 +0100

    vector methods

--- Vector/Vector/main.cpp
+++ Vector/Vector/main.cpp
@@ -23,5 +23,41 @@ int main()
 
 	printVector(vFloat);
 
+	/*---------------------------------------------------*/
+
+	vector<int> vInts;
+	vInts.assign(10, 5);
+	printVector(vInts);
+	assignFunction(vInts, 1);
+	pushBackFunction(vInts, 2);
+	emplaceFunction(vInts, 1, 3);
+	cout << endl;
+
+	/*---------------------------------------------------*/
+
+	vector<int> vectorInts;
+	vectorInts.push_back(0);
+	vectorInts.push_back(1);
+	vectorInts.push_back(2);
+	vectorInts.push_back(3);
+	vectorInts.push_back(4);
+	vectorInts.push_back(5);
+	vectorInts.push_back(6);
+	vectorInts.push_back(7);
+	printVector(vectorInts);
+	cout << endl;
+
+	vectorInts.erase(vectorInts.begin() + 1, vectorInts.begin() + 3);
+	printVector(vectorInts);
+	cout << endl;
+
+	vectorInts.pop_back();
+	printVector(vectorInts);
+	cout << endl;
+
+	vectorInts.clear();
+	printVector(vectorInts);
+
+
 	return 0;
 }
